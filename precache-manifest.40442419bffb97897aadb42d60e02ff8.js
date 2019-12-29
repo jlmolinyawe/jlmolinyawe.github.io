e87e3ca661de93b947b8091acde9b6451b48da71
Justin Molinyawe <joosen@local>
2019-12-29 11:16:49 -0800

    Updates

---
+++ precache-manifest.40442419bffb97897aadb42d60e02ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db6b15fc2495ff8b45da79798e80497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b580f58c981befa3dd87",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c60fc4ac968537677bc",
+    "url": "/static/js/2.cabc248b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cabc248b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b580f58c981befa3dd87",
+    "url": "/static/js/main.cc25d4cf.chunk.js"
+  },
+  {
+    "revision": "35ca06baed89b3bea97b",
+    "url": "/static/js/runtime-main.7bafd99e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
